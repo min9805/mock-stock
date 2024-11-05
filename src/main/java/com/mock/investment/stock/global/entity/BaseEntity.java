4d743e8bb9ca3a4e2f9588ecbe91b1f71be5a6e3
Mingyu <min1925k@gmail.com>
2024-11-05 17:04:47 +0900

    feat: BaseEntity 생성

---
+++ src/main/java/com/mock/investment/stock/global/entity/BaseEntity.java
@@ -0,0 +1,28 @@
+package com.mock.investment.stock.global.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
