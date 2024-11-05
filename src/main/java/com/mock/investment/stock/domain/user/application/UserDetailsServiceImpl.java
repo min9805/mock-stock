4f5de93a507cf203b550b15e35d8af728af53cfa
Mingyu <min1925k@gmail.com>
2024-11-05 17:02:26 +0900

    feat: Security 를 위한 UserDetailService 구현체 생성

---
+++ src/main/java/com/mock/investment/stock/domain/user/application/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.mock.investment.stock.domain.user.application;
+
+import com.mock.investment.stock.domain.user.dao.UserRepository;
+import com.mock.investment.stock.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + email));
+
+        return new UserDetailsImpl(user);
+    }
+}
