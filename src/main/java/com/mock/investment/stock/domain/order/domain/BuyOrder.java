8231b83cc237066b24163d92635abb1bc99bdd53
Mingyu <min1925k@gmail.com>
2024-11-06 23:23:25 +0900

    feat: 매수 주문 엔티티 생성

--- src/main/java/com/mock/investment/stock/domain/order/domain/BuyOrder.java
+++ src/main/java/com/mock/investment/stock/domain/order/domain/BuyOrder.java
@@ -2,10 +2,8 @@ package com.mock.investment.stock.domain.order.domain;
 
 import com.mock.investment.stock.domain.account.domain.Account;
 import com.mock.investment.stock.domain.stock.domain.Stock;
-import com.mock.investment.stock.domain.user.domain.User;
 import com.mock.investment.stock.global.entity.BaseEntity;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 import org.hibernate.annotations.Comment;
@@ -13,59 +11,59 @@ import org.hibernate.annotations.Comment;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "sell_orders")
+@Table(name = "buy_orders")
 @Getter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class SellOrder extends BaseEntity {
+public class BuyOrder extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long sellOrderId;
+    private Long buyOrderId;
 
     @PositiveOrZero
-    @Comment("매도 주문 수량")
-    private Integer sellQuantity;
+    @Comment("매수 주문 수량")
+    private Integer buyQuantity;
 
-    @Comment("매도 주문 상태")
+    @Comment("매수 주문 상태")
     private OrderStatus orderStatus;
 
-    @Comment("매도 주문 타입")
+    @Comment("매수 주문 타입")
     private OrderType orderType;
 
-    @Comment("매도 주문 가격")
-    private Double sellPrice;
+    @Comment("매수 주문 가격")
+    private Double buyPrice;
 
     @PositiveOrZero
-    @Comment("매도 체결 수량")
+    @Comment("매수 체결 수량")
     private Integer filledQuantity;
 
     @PositiveOrZero
-    @Comment("매도 주문 남은 수량")
+    @Comment("매수 주문 남은 수량")
     private Integer remainingQuantity;
 
-    @Comment("매도 체결 가격")
+    @Comment("매수 체결 가격")
     private Double executedPrice;
 
-    @Comment("매도 주문 수수료")
+    @Comment("매수 주문 수수료")
     private Double fee;
 
-    @Comment("매도 주문 만료 시간")
+    @Comment("매수 주문 만료 시간")
     private LocalDateTime expireAt;
 
-    @Comment("매도 주문 취소 시간")
+    @Comment("매수 주문 취소 시간")
     private LocalDateTime cancelledAt;
 
-    @Comment("매도 주문 완료 시간")
+    @Comment("매수 주문 완료 시간")
     private LocalDateTime completedAt;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id")
-    @Comment("매도 주문한 계좌")
+    @Comment("매수 주문한 계좌")
     private Account account;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "code")
-    @Comment("매도 주문한 주식")
+    @Comment("매수 주문한 주식")
     private Stock stock;
 }
