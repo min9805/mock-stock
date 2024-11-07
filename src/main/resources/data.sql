8687bf67be8bf8178937ab4aeb40bde719c861db
Mingyu <min1925k@gmail.com>
2024-11-07 22:08:52 +0900

    feat: 데이터베이스 init 설정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users
+(o_auth_provider, user_role, created_at, id, updated_at, email, name, password)
+VALUES
+    ('0', '1', '2024-11-07 12:44:27.570979', '1', NULL, 'admin@admin.com', 'admin', '$2a$10$IEewf3yfCKXxRTavDLA48eDBZPAWvza3m6/d6zcsDTZh/kZuZj0KS');
